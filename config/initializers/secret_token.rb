f5daab9300d5ca1477efc8b856c8ecf4367400f9
Azbest <azamat1000@gmail.com>
2017-03-22 17:00:36 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aza::Application.config.secret_key_base = '74f5c5a10c7725de4adb7ea5d9c8a791238c3b63e0f64713d6a35a52f5254b78490b4ad5e66ae8e94180058352c0aaa0c169e653de1d3f6a3385d6fe5fa0a6e9'
